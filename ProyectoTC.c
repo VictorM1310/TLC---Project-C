0c9620b39bb7553ee538977a0403949eb8b02da1
AValRS <77701887+AValRS@users.noreply.github.com>
2021-01-31 21:39:43 -0600

    Update ProyectoTC.c

--- ProyectoTC.c
+++ ProyectoTC.c
@@ -3,79 +3,62 @@
 #include <stdlib.h>
 #include "ArregloTokens.h"
 
-
-
 int CargaTokens(char *nomArchivo, arregloChar2D *aC)
 {
     FILE *arch;
     arch = fopen (nomArchivo, "r");
 	char linea[256];
     int i=0;
-   
-        
 	if (arch != NULL)
 	{
+
 		while(fgets(linea, 255, arch)!=NULL)
         {
             char *s, *ptr;
-
-        
             s = strtok_r(linea, " \t\n", &ptr);
-                
-            if (s)
+            if (s[0])
             {
                 do
                 {   if (linea[0] == '#')
                     {
                         break;
                     }
-                    strncpy (aC->A[i], s, 255);
-                    printf("En esta posicion se encuentra: %s\n",aC->A[i]);
-                    
-                    
+                    if(s[0]=='"')
+                    {
+                        char arrayTemp[256];
+                        do
+                        {
+                            strcat(arrayTemp,s);
+                            s = strtok_r(NULL, " \t\n", &ptr);
+                            strcat(arrayTemp, " ");
 
-                    s = strtok_r(NULL, " \t\n", &ptr);
-                    
-
+                        }while(s[0]!='"');
+                        strcat(arrayTemp,s);
+                        strncpy (aC->A[i], arrayTemp, 255);
+                        memset(arrayTemp,0,256);
+                        s = strtok_r(linea, " \t\n", &ptr);
+                        printf("Esta es la posicion %d: %s\n",i,aC->A[i]);
+                    }
                     i++;
+                    strncpy (aC->A[i], s, 255);
+                    printf("Esta es la posicion %d: %s\n",i,aC->A[i]);
+                    s = strtok_r(NULL, " \t\n", &ptr);
 
                 } while (s != NULL);
                 printf("\n");
             }
             else
                 printf("No hay token que leer\n");
-        }   
-
-        fclose(arch);
-	}
-
-	return 0;
-}
-
-void Comp(arregloChar2D *aC)
-{
-   char *palabras[] = {"PROGRAMA","factorial", "REPITE", "VECES", "FINREPITE", "LEE", "FINPROG", "IMPRIME"};
-
-   for(int j = 0; j < 37; j++)
-    for(int k = 0; k < 8; k++)
-    {   
-        printf("%s con %s \n\n",palabras[k], aC->A[j]);
-
-        if(strncmp(aC->A[j], palabras[k],255) == 0)
-        {
-            printf("%s es palabra aceptada\n\n", aC->A[j]);
 
         }
-    }
-    
+	}
+	return 0;
 }
-
-
 
 int main(int argc, char **argv)
 {
     arregloChar2D Q;
-    unsigned int r = 100, c = 100;
+    unsigned int r = 40, c = 100;
     initArregloChar2D(&Q,r,c);
 
 	if (argc>1)
@@ -88,9 +71,6 @@ int main(int argc, char **argv)
       fprintf(stderr, "Error: Se tiene que pasar como argumento el archivo que contiene la base de datos\n");
       exit(1);
     }
-
-    Comp(&Q);
     imprimeArregloC2D(&Q);
-    
-   return 0;
+    return 0;
 }
