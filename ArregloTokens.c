fa4a809452cff5b3cd4e02a9b996f1a1163519dd
AValRS <77701887+AValRS@users.noreply.github.com>
2021-02-09 16:06:25 -0600

    Update ArregloTokens.c

--- ArregloTokens.c
+++ ArregloTokens.c
@@ -40,8 +40,6 @@ int initArregloChar2D(arregloChar2D *aC, unsigned int r, unsigned int c)
 	return 0;
 }
 
-
-
 void liberaArregloChar2D(arregloChar2D *aC)
 {
 		if(aC->tama)
@@ -52,19 +50,68 @@ void liberaArregloChar2D(arregloChar2D *aC)
 			aC->A = NULL;
 		}
 }
+int initArregloInt2D(arregloInt2D *aI, unsigned int r1, unsigned int c1)
+{
+	if (r1 && c1) //Validamos que r y c sean diferentes de 0
+	{
+		unsigned int i;
+
+		aI->B = malloc(r1 * sizeof (int *));
+		if (aI->B != NULL) //equivalente a aI->A
+		{
+			aI->B[0] = malloc(r1 * c1 * sizeof (int));
+			if (aI->B[0] != NULL) //equivalente a aI->A[0]
+				for (i = 1; i < r1; ++i)
+					//aI->A[i] = &(aI->A[0][i * c]);//Aqui usamos el operador &
+					aI->B[i] = aI->B[0] + i * c1;//Aqui usamos aritmetica de apuntadores.
+			else
+			{
+				free(aI->B);
+				aI->B = NULL;
+				aI->ren = aI->col = aI->tama = 0;
+				return -1;
+			}
+		}
+		else
+		{
+			aI->ren = aI->col = aI->tama = 0;
+			return -1;
+		}
+		aI->ren = r1;
+		aI->col = c1;
+		aI->tama = r1 * c1;
+		return 0;
+	}
+	aI->ren = aI->col = aI->tama = 0;
+	aI->B = NULL;
+	return 0;
+}
+
+
+
+void liberaArregloInt2D(arregloInt2D *aI)
+{
+		if(aI->tama)
+		{
+			free(aI->B[0]);
+			free(aI->B);
+			aI->ren = aI->col = aI->tama = 0;
+			aI->B = NULL;
+		}
+}
 
 void fImprimeArregloC2D(FILE* arch, arregloChar2D *aC)
 {
 	unsigned int i;
 
-	
-	for (i = 0; i < aC->ren; ++i)
-	{	
+
+	for (i = 0; i < 50; ++i)
+	{
 		fprintf(arch, "[");
 		fprintf(arch, " %s ", aC->A[i]);
 		fprintf(arch, "];\n");
 	}
-	
+
 }
 
 void imprimeArregloC2D(arregloChar2D *aC)
@@ -72,10 +119,3 @@ void imprimeArregloC2D(arregloChar2D *aC)
 	fImprimeArregloC2D(stdout, aC);
 }
 
-/*void imprimeArregloC2D(arregloChar2D *aC)
-{
-	for(int i = 0; i < 50;i++)
-	{		printf("C[%d] = %s\n\n",i,aC->A[i]);
-			
-	}
-}*/	
