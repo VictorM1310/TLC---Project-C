0b1f735689d741d6c2786acef20c820868bb9279
AValRS <77701887+AValRS@users.noreply.github.com>
2021-02-09 16:07:11 -0600

    Update ArregloTokens.h

--- ArregloTokens.h
+++ ArregloTokens.h
@@ -1,4 +1,4 @@
-#ifndef __ARREGLO_TOKENS__ 
+#ifndef __ARREGLO_TOKENS__
 #define __ARREGLO_TOKENS__
 
 #include <stdlib.h>
@@ -7,23 +7,34 @@
 typedef struct
 {
 	char **A;           //Apuntador al arreglo.
-	unsigned int ren;  //Número de renglones de la matriz
-	unsigned int col;  //Número de columnas de la matriz
-	unsigned int tama; //Número de elementos de la matriz (ren x col).
+	unsigned int ren;  //Numero de renglones de la matriz
+	unsigned int col;  //Numero de columnas de la matriz
+	unsigned int tama; //Numero de elementos de la matriz (ren x col).
 
 } arregloChar2D;
 
+typedef struct
+{
+	int **B;
+	unsigned int ren;
+	unsigned int col;
+	unsigned int tama;
 
+} arregloInt2D;
 
 int initArregloChar2D(arregloChar2D *aC, unsigned int r, unsigned int c);
 
+int initArregloInt2D(arregloInt2D *aI, unsigned int r1, unsigned int c1);
+
 void liberaArregloChar2D(arregloChar2D *aC);
+
+void liberaArregloInt2D(arregloInt2D *aI);
 
 void fImprimeArregloC2D(FILE *arch, arregloChar2D *aC);
 
 void imprimeArregloC2D(arregloChar2D *aC);
 
-int CargaTokens(char *nomArchivo, arregloChar2D *aC);
+int CargaTokens(char *nomArchivo, char Tokens[][256], arregloInt2D *aI);
 
-void Comp(arregloChar2D *aC);
+
 #endif
